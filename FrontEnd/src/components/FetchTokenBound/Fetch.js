5c704a3ba5fd3b06ef20c9c749481e791213899a
mxber2022 <mxber2022@gmail.com>
2023-09-02 10:14:05 +0200

    config-overrides

--- FrontEnd/src/components/FetchTokenBound/Fetch.js
+++ FrontEnd/src/components/FetchTokenBound/Fetch.js
@@ -1,4 +1,8 @@
 import "./Fetch.css";
+import { RequestNetwork } from '@requestnetwork/request-client.js';
+import { Web3SignatureProvider } from '@requestnetwork/web3-signature';
+import { payRequest } from '@requestnetwork/payment-processor';
+
 //const xprovider = new ethers.providers.JsonRpcProvider("");
 
 function Fetch () {
