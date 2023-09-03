27fe2f4cdda77b763db2a1b858d2c3edcd2d35ff
mxber2022 <mxber2022@gmail.com>
2023-09-03 05:48:40 +0200

    v1 update frontend

--- FrontEnd/src/components/FetchTokenBound/Fetch.js
+++ FrontEnd/src/components/FetchTokenBound/Fetch.js
@@ -8,7 +8,9 @@ import { hasSufficientFunds, utils } from "@requestnetwork/payment-processor";
 import { approveErc20, hasErc20Approval } from "@requestnetwork/payment-processor";
 import { payRequest } from "@requestnetwork/payment-processor";
 import { useState } from "react";
-
+import Metals from "./Metals.jpeg";
+import mortgage from "./mortgage.jpeg";
+import Patent from "./Patent.jpeg";
 
 function Fetch () {
     //const xprovider = new ethers.providers.JsonRpcProvider(window.ethereum);
@@ -24,7 +26,7 @@ function Fetch () {
     });
 
 
-    const payeeIdentity = '0x7199D548f1B30EA083Fe668202fd5E621241CC89';
+    const payeeIdentity = '0xc614d9a56c9EaFbFc50B993F70386b0fb0B37A24';
     //const payerIdentity = '0x7199D548f1B30EA083Fe668202fd5E621241CC89';
     const [payerIdentity, setPayerIdentity] = useState('');
     const handlePayerIdentityChange = (e) => {
@@ -134,29 +136,143 @@ function Fetch () {
     }
 
 
-    return(
+    return (
         <>
-            <h1> Mantle X Request</h1>
-
-            <div>
-                <label htmlFor="payerIdentity">Payee</label>
-                <input type="text" id="payerIdentity" name="payerIdentity" value={payerIdentity} onChange={handlePayerIdentityChange} />
+        {/* Content #1 */}
+        <div className="fetch-container">
+            <div className="left-content">
+                <img
+                    src={Metals}
+                    alt="Your Image"
+                    style={{ width: "50%", height: "40%", padding: "20px" }}
+                />
             </div>
 
-            <div>
-                <label htmlFor="reason">Reason:</label>
-                <input type="text" id="reason" name="reason" value={reason} onChange={handleReasonChange} />
+            <div className="right-content">
+                <p>Our Tokenized Gold and Silver Contracts bring a new dimension to the market by combining the purity standards of traditional precious metals with the revolutionary concept of non-fungible tokens (NFTs). These digital assets represent ownership in real-world gold and silver, boasting a minimum fineness of 99.9% for gold and 99.5% for silver. With standardized tick sizes, expiration dates, and the unique NFT characteristics, investors can access a transparent, secure, and highly liquid market, bridging the traditional and digital realms of precious metal trading.</p>
+
+                {/* Input Fields and Buttons */}
+                <div>
+                    <label htmlFor="payerIdentity">Payee: </label>
+                    <input
+                        type="text"
+                        id="payerIdentity"
+                        name="payerIdentity"
+                        value={payerIdentity}
+                        onChange={handlePayerIdentityChange}
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="reason">Reason: </label>
+                    <input
+                        type="text"
+                        id="reason"
+                        name="reason"
+                        value={reason}
+                        onChange={handleReasonChange}
+                    />
+                </div>
+
+                <button onClick={createReq}>Request for Invoice</button>
+
+                {requestId && <p>{requestId}</p>}
+
+                <button onClick={payReq}>Pay the request</button>
             </div>
-    
-            <button onClick={createReq}>Request for Invoice</button>
+        </div>
 
-            {
-                <p>{requestId}</p>
-            }   
+        {/* Content #2 (Duplicate) */}
+        <div className="fetch-container">
+            <div className="left-content">
+                <img
+                    src={Patent}
+                    alt="Your Image"
+                    style={{ width: "50%", height: "40%", padding: "20px" }}
+                />
+            </div>
 
-            <button onClick={payReq}> Pay the request</button>
-        </> 
+            <div className="right-content">
+                <p>Introducing our Tokenized Technological Patent Contracts, where innovation meets the blockchain era. These groundbreaking contracts offer a unique fusion of traditional patent rights and cutting-edge non-fungible tokens (NFTs). Each contract encapsulates the essence of groundbreaking inventions, protected by the strength of blockchain technology. With standardized terms, transparent ownership records, and the added dimension of NFTs, inventors and tech enthusiasts alike can confidently navigate this new realm of patent trading, unlocking a future where ideas and innovation find their digital home.</p>
+
+                {/* Input Fields and Buttons */}
+                <div>
+                    <label htmlFor="payerIdentity">Payee: </label>
+                    <input
+                        type="text"
+                        id="payerIdentity"
+                        name="payerIdentity"
+                        value={payerIdentity}
+                        onChange={handlePayerIdentityChange}
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="reason">Reason: </label>
+                    <input
+                        type="text"
+                        id="reason"
+                        name="reason"
+                        value={reason}
+                        onChange={handleReasonChange}
+                    />
+                </div>
+
+                <button onClick={createReq}>Request for Invoice</button>
+
+                {requestId && <p>{requestId}</p>}
+
+                <button onClick={payReq}>Pay the request</button>
+            </div>
+        </div>
+
+        {/* Content #3 (Duplicate) */}
+        <div className="fetch-container">
+            <div className="left-content">
+                <img
+                    src={mortgage}
+                    alt="Your Image"
+                    style={{ width: "50%", height: "40%", padding: "20px" }}
+                />
+            </div>
+
+            <div className="right-content">
+                <p>Introducing our Tokenized Mortgage Contracts, revolutionizing the way we think about homeownership. These visionary contracts seamlessly merge the traditional concept of mortgages with the power of blockchain and non-fungible tokens (NFTs). Each contract represents a unique piece of real estate, secured and immortalized on the blockchain. With standardized terms, transparent property records, and the added layer of NFTs, homeowners and investors can explore a novel world of real estate investment, paving the way for a future where property ownership is as easy as trading digital assets.</p>
+
+                {/* Input Fields and Buttons */}
+                <div>
+                    <label htmlFor="payerIdentity">Payee: </label>
+                    <input
+                        type="text"
+                        id="payerIdentity"
+                        name="payerIdentity"
+                        value={payerIdentity}
+                        onChange={handlePayerIdentityChange}
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="reason">Reason: </label>
+                    <input
+                        type="text"
+                        id="reason"
+                        name="reason"
+                        value={reason}
+                        onChange={handleReasonChange}
+                    />
+                </div>
+
+                <button onClick={createReq}>Request for Invoice</button>
+
+                {requestId && <p>{requestId}</p>}
+
+                <button onClick={payReq}>Pay the request</button>
+            </div>
+        </div>
+
+        {/* ... (rest of your component) */}
+        </>
     );
 }
-
+    
 export default Fetch;
--- FrontEnd/src/components/Nav/Nav.js
+++ FrontEnd/src/components/Nav/Nav.js
@@ -1,15 +1,17 @@
 import { Web3Button } from '@web3modal/react'
 import "./Nav.css";
-function Nav () {
-    return(
-        <>
-            <nav className="nav">
-                <div>
-                    <Web3Button />
-                </div>              
-            </nav>
-        </>
-    );
+
+function Nav() {
+  return (
+    <div className="nav">
+      <div className="top-left">
+        <p>Asset Link</p>
+      </div>
+      <div className="top-right">
+        <Web3Button />
+      </div>
+    </div>
+  );
 }
 
 export default Nav;
